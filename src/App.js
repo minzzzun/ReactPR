ff67f30bc72d93e4a4fdbdcbb5a71108bc5f021b
minjun kim <minzzzun@gmail.com>
2024-11-11 10:34:19 +0900

    JSX 연습

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Comp1 from './Comp1';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
+
+        <div className="black-nav">
+        <h4>블로그임</h4>
+      </div>
+        <Comp1/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
+// eslint-disable-next-line 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+const myElement1= <h1>React is {5 + 5} times better with JSX</h1>;
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const myElement4 = React.createElement('h1', {}, 'I do not use JSX!'); //JSX 사용하지 않고 만들기 
+const myElement5 = (
+  <>
+  <span className="date">오늘의 날짜 :</span> <input type="text" />
+  </>);
+const myElement3 = (
+  <ul>
+  <li>Apples</li>
+  <li>Bananas</li>
+  <li>Cherries</li>
+  </ul>
+ );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  // myElement
+  
+
+  // myElement3
+  <>
+  {myElement4}
+  {myElement5}
+  <div style={ {color : 'blue', fontSize : '30px'} }> 글씨 </div>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
